a3de35e987af461157faecd8c68b22e41fa72e60
GoutamSamal9 <gsamal101@gmail.com>
2020-10-28 14:43:39 +0530

    Updates

---
+++ precache-manifest.55424b25b7d871ebc5726256d366ba13.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5b89900798762d4bbad60d5bbf13884d",
+    "url": "/CountryList/index.html"
+  },
+  {
+    "revision": "db3a4b02851b57b80a2e",
+    "url": "/CountryList/static/css/main.f7c042f3.chunk.css"
+  },
+  {
+    "revision": "4b971e8724e271e8f6e7",
+    "url": "/CountryList/static/js/2.9fe68515.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/CountryList/static/js/2.9fe68515.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "db3a4b02851b57b80a2e",
+    "url": "/CountryList/static/js/main.ec397726.chunk.js"
+  },
+  {
+    "revision": "db0835fd455e1976271a",
+    "url": "/CountryList/static/js/runtime-main.97abc2ad.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/CountryList/static/media/logo.5d5d9eef.svg"
+  }
+]);
